17c02a86bcdd640b6a229642e168e4cb8c162bf0
Kriticos <kriticosl@gmail.com>
2025-11-15 04:45:10 -0300

    feat: create 01-create-databases.sql

---
+++ stack/ctr-mysql/mysql-init/01-create-databases.sql
@@ -0,0 +1,4 @@
+-- Criação dos bancos usados pelos serviços
+CREATE DATABASE IF NOT EXISTS glpi CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS grafana CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
